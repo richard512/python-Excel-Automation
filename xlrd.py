3fbdd6f28d26c15fb3350b1a8302f42c9b8b7125
richard512 <richard512@users.noreply.github.com>
2017-06-17 11:31:44 -0700

    Create xlrd.py

---
+++ xlrd.py
@@ -0,0 +1,35 @@
+import xlrd
+ 
+#----------------------------------------------------------------------
+def open_file(path):
+    """
+    Open and read an Excel file
+    """
+    book = xlrd.open_workbook(path)
+ 
+    # print number of sheets
+    print book.nsheets
+ 
+    # print sheet names
+    print book.sheet_names()
+ 
+    # get the first worksheet
+    first_sheet = book.sheet_by_index(0)
+ 
+    # read a row
+    print first_sheet.row_values(0)
+ 
+    # read a cell
+    cell = first_sheet.cell(0,0)
+    print cell
+    print cell.value
+ 
+    # read a row slice
+    print first_sheet.row_slice(rowx=0,
+                                start_colx=0,
+                                end_colx=2)
+ 
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    path = "test.xls"
+    open_file(path)
