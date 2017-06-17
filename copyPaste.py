9489ce1fab8ce08df01b06633c48b8d1cdfc2774
richard512 <richard512@users.noreply.github.com>
2017-06-17 11:56:07 -0700

    Create copyPaste.py

---
+++ copyPaste.py
@@ -0,0 +1,11 @@
+from win32com.client import Dispatch
+wkbk1 = "...\workbook1.xlsx"
+wkbk2 = "...\workbook2.xlsx"
+excel = Dispatch("Excel.Application")
+excel.Visible = 1
+source = excel.Workbooks.Open(wkbk1)
+excel.Range("A1:A3").Select()
+excel.Selection.Copy()
+copy = excel.Workbooks.Open(wkbk2)
+excel.Range("A1:A3").Select()
+excel.Selection.PasteSpecial(Paste=-4163)
