c1d24028aab038c0291c47b8d44bc898db063214
richard512 <richard512@users.noreply.github.com>
2017-06-18 10:37:10 -0700

    Update runMacro.py

--- runMacro.py
+++ runMacro.py
@@ -1,13 +1,13 @@
 import os
 import win32com.client
 
-excelfile = "C:\Full Location\To\excelsheet.xlsm"
+excelFile = "C:\Full Location\To\excelsheet.xlsm"
 macroToRun = "excelsheet.xlsm!modulename.macroname"
 
-if os.path.exists(excelfile):
+if os.path.exists(excelFile):
     xl = win32com.client.Dispatch("Excel.Application")
     
-    xl.Workbooks.Open(Filename=excelfile, ReadOnly=1)
+    xl.Workbooks.Open(Filename=excelFile, ReadOnly=1)
     # remove ", ReadOnly=1" for file save
     
     xl.Application.Run(macroToRun)
