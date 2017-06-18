0145c2d04e5ac8cbf141d6b5f427395c20229792
richard512 <richard512@users.noreply.github.com>
2017-06-18 15:09:49 -0700

    Update copyPaste.py

--- copyPaste.py
+++ copyPaste.py
@@ -1,11 +1,30 @@
 from win32com.client import Dispatch
-wkbk1 = "worksheet1.xlsx"
-wkbk2 = "worksheet2.xlsx"
+wkbk1 = "C:\\path\\to\\worksheet1.xlsx"
+wkbk2 = "C:\\path\\to\\worksheet2.xlsx"
+wkbk3 = "C:\\path\\to\\worksheet3.xlsx"
 excel = Dispatch("Excel.Application")
 excel.Visible = 1
+excel.ScreenUpdating = False
+
+# copy from source
 source = excel.Workbooks.Open(wkbk1)
-excel.Range("A1:A3").Select()
+excel.Range("A2:C4").Select()
 excel.Selection.Copy()
-copy = excel.Workbooks.Open(wkbk2)
-excel.Range("A1:A3").Select()
-excel.Selection.PasteSpecial(Paste=-4163)
+
+# paste (appended) to target
+target = excel.Workbooks.Open(wkbk2)
+
+ws = target.sheets(1)
+used = ws.UsedRange
+maxrow = used.Row + used.Rows.Count - 1
+maxcol = used.Column + used.Columns.Count - 1
+newrow = maxrow + 1
+
+destrange = "A"+str(newrow) #+":C9"
+print(destrange)
+
+excel.Range(destrange).Select()
+excel.Selection.PasteSpecial(Paste=-4122)
+
+excel.ScreenUpdating = True
+#ws.SaveAs(wkbk3)
