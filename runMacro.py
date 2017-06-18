887a0ee4103c7336291be5c3e05bcfaba055d012
richard512 <richard512@users.noreply.github.com>
2017-06-18 10:36:42 -0700

    Update runMacro.py

--- runMacro.py
+++ runMacro.py
@@ -1,10 +1,17 @@
 import os
 import win32com.client
 
-if os.path.exists("excelsheet.xlsm"):
-    xl=win32com.client.Dispatch("Excel.Application")
-    xl.Workbooks.Open(Filename="C:\Full Location\To\excelsheet.xlsm", ReadOnly=1)
-    xl.Application.Run("excelsheet.xlsm!modulename.macroname")
-##    xl.Application.Save() # if you want to save then uncomment this line and change delete the ", ReadOnly=1" part from the open function.
+excelfile = "C:\Full Location\To\excelsheet.xlsm"
+macroToRun = "excelsheet.xlsm!modulename.macroname"
+
+if os.path.exists(excelfile):
+    xl = win32com.client.Dispatch("Excel.Application")
+    
+    xl.Workbooks.Open(Filename=excelfile, ReadOnly=1)
+    # remove ", ReadOnly=1" for file save
+    
+    xl.Application.Run(macroToRun)
+    #xl.Application.Save() # uncomment for file save
+    
     xl.Application.Quit() # Comment this out if your excel script closes
     del xl
