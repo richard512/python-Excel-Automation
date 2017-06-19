ef06dace9b01bcc8166767f64f869e3cd840aeb3
richard512 <richard512@users.noreply.github.com>
2017-06-19 16:10:21 -0700

    Update openpyxlTest.py

--- openpyxlTest.py
+++ openpyxlTest.py
@@ -1,28 +1,26 @@
+"""
+Uses openpyxl to generate "numbers.xlsx" (a basic spreadsheet)
+"""
+
 import openpyxl as px
-import numpy as np
 
-W = px.load_workbook('filename.xlsx', use_iterators = True)
-p = W.get_sheet_by_name(name = 'Sheet1')
+WW = px.Workbook()
+pp = WW.get_active_sheet()
+pp.title = 'SheetNameHere'
 
-a=[]
+f = {'A':1,'B':2,'C':3,'D':4,'E':5,'F':6}
 
-for row in p.iter_rows():
-    for k in row:
-        a.append(k.internal_value)
-
-# convert list a to matrix (for example 5*6)
-aa= np.resize(a, [5, 6])
-
-# save matrix aa as xlsx file
-WW=px.Workbook()
-pp=WW.get_active_sheet()
-pp.title='NEW_DATA'
-
-f={'A':0,'B':1,'C':2,'D':3,'E':4,'F':5}
-
-#insert values in six columns
 for (i,j) in f.items():
-    for k in np.arange(1,len(aa)+1):
-        pp.cell('%s%d'%(i,k)).value=aa[k-1][j]
+	coord = str(i)+str(1)
+	val = "col" + str(i)
+	print (coord, " = ", val)
+	pp.cell(coord).value = val
 
-WW.save('newfilname.xlsx')
+for (i,j) in f.items():
+    for k in (1, 2, 3):
+        #pp.cell('%s%d'%(i,k)).value= k
+        coord = str(i)+str(k+1)
+        print (coord, " = ", k)
+        pp.cell(coord).value = k
+
+WW.save('numbers.xlsx')
