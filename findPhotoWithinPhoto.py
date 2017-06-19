6a14831cb53bbfecc2d0972bbc89941a4f5c9878
richard512 <richard512@users.noreply.github.com>
2017-06-19 11:52:14 -0700

    Update findPhotoWithinPhoto.py

--- findPhotoWithinPhoto.py
+++ findPhotoWithinPhoto.py
@@ -3,23 +3,26 @@ Uses OpenCV to find what part of an image (large_image.png) contains another ima
 Yellow Circle = Center of the location of small_image.png within large_image.png. The spot to click.
 '''
 
-import cv2
 
+import cv2
+'''
 if cv2.__version__[:1] == '2':
 	import cv2 as cv
 	method = cv2.TM_SQDIFF_NORMED
 elif cv2.__version__[:1] == '3':
-	import cv2 as cv
-	method = cv2.TM_SQDIFF_NORMED
+'''
+
+#import cv2 as cv
+method = cv2.TM_SQDIFF_NORMED
 
 # Read the images from the file
-small_image = cv.imread('small_image.png')
-large_image = cv.imread('large_image.png')
+small_image = cv2.imread('small_image.png')
+large_image = cv2.imread('large_image.png')
 
-result = cv.matchTemplate(small_image, large_image, method)
+result = cv2.matchTemplate(small_image, large_image, method)
 
 # We want the minimum squared difference
-mn,_,mnLoc,_ = cv.minMaxLoc(result)
+mn,_,mnLoc,_ = cv2.minMaxLoc(result)
 
 # Draw the rectangle:
 # Extract the coordinates of our best match
@@ -47,10 +50,10 @@ color = (0,255,255)
 cv2.circle(large_image, center, radius, color, thickness=10, lineType=8, shift=0)
 
 # Step 3: Draw the rectangle on large_image
-cv.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
 # Display the original image with the rectangle around the match.
-cv.imshow('output',large_image)
+cv2.imshow('output',large_image)
 
 # The image is only displayed if we call this
-cv.waitKey(0)
+cv2.waitKey(0)
