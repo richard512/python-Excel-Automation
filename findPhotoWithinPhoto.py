60bc61ffe4d90a8941b010cbd8eabd0efa8f0f97
richard512 <richard512@users.noreply.github.com>
2017-06-19 12:02:59 -0700

    Update findPhotoWithinPhoto.py

--- findPhotoWithinPhoto.py
+++ findPhotoWithinPhoto.py
@@ -3,57 +3,48 @@ Uses OpenCV to find what part of an image (large_image.png) contains another ima
 Yellow Circle = Center of the location of small_image.png within large_image.png. The spot to click.
 '''
 
-
 import cv2
-'''
-if cv2.__version__[:1] == '2':
-	import cv2 as cv
-	method = cv2.TM_SQDIFF_NORMED
-elif cv2.__version__[:1] == '3':
-'''
-
-#import cv2 as cv
 method = cv2.TM_SQDIFF_NORMED
 
-# Read the images from the file
+# get the images
 small_image = cv2.imread('small_image.png')
 large_image = cv2.imread('large_image.png')
 
+# look for small_image inside large_image
 result = cv2.matchTemplate(small_image, large_image, method)
 
-# We want the minimum squared difference
+# minimum squared difference
 mn,_,mnLoc,_ = cv2.minMaxLoc(result)
 
-# Draw the rectangle:
-# Extract the coordinates of our best match
+# Coordinates of our best match
 MPx,MPy = mnLoc
 
-# Step 2: Get the size of the template. This is the same size as the match.
+# Get the size of the template. Same size as the match.
 trows,tcols = small_image.shape[:2]
 
-# TOP LEFT circle
+# draw TOP LEFT circle
 center = (MPx,MPy)
 radius = 5
 color = (0,255,0)
 cv2.circle(large_image, center, radius, color, thickness=1, lineType=8, shift=0)
 
-# BOTTOM RIGHT circle
+# draw BOTTOM RIGHT circle
 center = (MPx+tcols,MPy+trows)
 radius = 5
 color = (0,255,0)
 cv2.circle(large_image, center, radius, color, thickness=1, lineType=8, shift=0)
 
-# CENTER circle
-center = (MPx+tcols/2,MPy+trows/2)
+# draw CENTER circle
+center = (MPx+tcols//2,MPy+trows//2)
 radius = 5
 color = (0,255,255)
 cv2.circle(large_image, center, radius, color, thickness=10, lineType=8, shift=0)
 
-# Step 3: Draw the rectangle on large_image
+# draw red rectangle
 cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
-# Display the original image with the rectangle around the match.
+# display the results
 cv2.imshow('output',large_image)
 
-# The image is only displayed if we call this
+# wait for key press with 0 delay
 cv2.waitKey(0)
