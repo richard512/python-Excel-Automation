42c7006059e518e1fd770dc60cb77f1dc17bbc8c
richard512 <richard512@users.noreply.github.com>
2017-06-19 15:28:21 -0700

    Update and rename xlrd.py to xlrdtest.py

--- xlrdtest.py
+++ xlrdtest.py
@@ -1,5 +1,7 @@
 import xlrd
- 
+
+print (xlrd.__file__)
+
 #----------------------------------------------------------------------
 def open_file(path):
     """
@@ -8,28 +10,28 @@ def open_file(path):
     book = xlrd.open_workbook(path)
  
     # print number of sheets
-    print book.nsheets
+    print (book.nsheets)
  
     # print sheet names
-    print book.sheet_names()
+    print (book.sheet_names())
  
     # get the first worksheet
     first_sheet = book.sheet_by_index(0)
  
     # read a row
-    print first_sheet.row_values(0)
+    print (first_sheet.row_values(0))
  
     # read a cell
     cell = first_sheet.cell(0,0)
-    print cell
-    print cell.value
+    print (cell)
+    print (cell.value)
  
     # read a row slice
-    print first_sheet.row_slice(rowx=0,
+    print (first_sheet.row_slice(rowx=0,
                                 start_colx=0,
-                                end_colx=2)
+                                end_colx=2))
  
 #----------------------------------------------------------------------
 if __name__ == "__main__":
-    path = "test.xls"
+    path = "worksheet1.xlsx"
     open_file(path)
