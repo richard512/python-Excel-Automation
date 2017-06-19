9d87786e8f5b1162118cb62b087ba7db207d0d2d
richard512 <richard512@users.noreply.github.com>
2017-06-19 00:07:22 -0700

    Add files via upload

--- findPhotoWithinPhoto.py
+++ findPhotoWithinPhoto.py
@@ -1,16 +1,20 @@
 import cv2
-from cv2 import cv
 
-method = cv.CV_TM_SQDIFF_NORMED
+if cv2.__version__[:1] == '2':
+	from cv2 import cv
+	method = cv.CV_TM_SQDIFF_NORMED
+elif cv2.__version__[:1] == '3':
+	import cv2 as cv
+	method = cv2.TM_SQDIFF_NORMED
 
 # Read the images from the file
-small_image = cv2.imread('small_image.png')
-large_image = cv2.imread('large_image.png')
+small_image = cv.imread('small_image.png')
+large_image = cv.imread('large_image.png')
 
-result = cv2.matchTemplate(small_image, large_image, method)
+result = cv.matchTemplate(small_image, large_image, method)
 
 # We want the minimum squared difference
-mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+mn,_,mnLoc,_ = cv.minMaxLoc(result)
 
 # Draw the rectangle:
 # Extract the coordinates of our best match
@@ -20,10 +24,10 @@ MPx,MPy = mnLoc
 trows,tcols = small_image.shape[:2]
 
 # Step 3: Draw the rectangle on large_image
-cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+cv.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
 # Display the original image with the rectangle around the match.
-cv2.imshow('output',large_image)
+cv.imshow('output',large_image)
 
 # The image is only displayed if we call this
-cv2.waitKey(0)
+cv.waitKey(0)
